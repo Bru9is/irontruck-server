da49e8c88174437872b75e2b38e977901b6de721
Bru9is <bru9is@outlook.com>
2022-08-18 22:18:14 -0300

    postRouters

--- models/Post.model.js
+++ models/Post.model.js
@@ -2,6 +2,7 @@ import mongoose from 'mongoose'
 const { Schema, model } = mongoose
 
 const postSchema = new Schema({
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
     date: { type: Date, required: true },
     origin: { type: String, required: true },
     destination: { type: String, required: true },
--- routes/post.routes.js
+++ routes/post.routes.js
@@ -10,10 +10,10 @@ postRouter.post('/new-post', isAuthenticated, attachCurrentUser, async (req, res
     try {
         const newPost = req.body
         const user = req.currentUser
-
+        
         if (user.role === 'user') {
-            const post = await Post.create(newPost)
-            return res.status(201).json(newPost)
+            const post = await Post.create({...newPost, userId: user._id})
+            return res.status(201).json(post)
         }
         else return res.status(401).json({ msg: 'Only users can post' });
     }
@@ -33,5 +33,40 @@ postRouter.get('/all-posts', isAuthenticated, async (req, res) => {
     }
 })
 
-export default postRouter
+postRouter.put("/edit-post/:postId", isAuthenticated, attachCurrentUser, async (req, res) => {
+    try {
+        const { postId } = req.params;
+        const userId = req.currentUser._id;
+        const foundPost = await Post.findOne({_id: postId})
+            
+        if (foundPost && foundPost.userId.toString() === userId.toString()) {
+                const updatedPost = await Post.findByIdAndUpdate({_id: postId}, req.body, { new: true })
+                return res.status(200).json(updatedPost)
+            } else {
+                return res.status(401).json({ msg: 'You are not authorized to edit this post or the post doesn´t exist' })
+            }
+    } catch (err) {
+        console.log(err)
+        return res.status(500).json({ msg: JSON.stringify(err) });
+    }
+})
 
+postRouter.delete("/delete-post/:postId", isAuthenticated, attachCurrentUser, async (req, res) => {
+    try {
+        const { postId } = req.params;
+        const userId = req.currentUser._id;
+        const foundPost = await Post.findOne({_id: postId})
+
+        if (foundPost && foundPost.userId.toString() === userId.toString()) {
+                const deletedPost = await Post.findOneAndDelete({_id: postId})
+                return res.status(204).json()
+            } else {
+                return res.status(401).json({ msg: 'You are not authorized to edit this post or the post doesn´t exist' })
+            }
+    } catch (err) {
+        console.log(err)
+        return res.status(500).json({ msg: JSON.stringify(err) });
+    }
+})
+
+export default postRouter
